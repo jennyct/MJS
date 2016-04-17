b54c9ae1e386d44fd85a9c9a1eedf4cb47b629f7
Stephen Em <stphnem@gmail.com>
2016-04-17 16:04:27 -0700

    Validate name, city, zip code, and phone number

--- public/js/hover.js
+++ public/js/hover.js
@@ -6,3 +6,60 @@ for (var i=0; i < sml.length; i++) {
 		big.src = this.src;
 	})
 }
+
+var formController = (function() {
+
+    var form = document.querySelector('form');
+    var firstName = document.querySelector('input[name=firstName]');
+    var lastName = document.querySelector('input[name=lastName]');
+    var areaCode = document.querySelector('#areaCode');
+    var firstNum = document.querySelector('#firstNum');
+    var secondNum = document.querySelector('#secondNum');
+    var city = document.querySelector('input[name=city');
+    var ccCSC = document.querySelector('#ccCSC');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        /* Validate form
+           If false, bring up alert
+           Else bring up email client 
+        */
+        if( !_isText(firstName) )
+            _alert("Enter a valid first name. No numbers.");
+        if( !_isText(lastName) )
+            _alert("Enter a valid last name. No numbers.");
+        if( !_isText(city) )
+            _alert("Enter a valid city. No numbers");
+        if( !_isValidZip() )
+            _alert("Enter a valid zipcode");
+        if( !_isValidPhone() ) 
+            _alert("Enter a valid phone number");
+
+    });
+
+    function _isText(element) {
+        var text = element.value;
+        return /^[a-z]+$/i.test(text);
+    }
+
+    function _isNum(element) {
+        var text = element.value;
+        return /^[0-9]+$/.test(text);
+    }
+
+    function _isValidZip() {
+        var zipcode = document.querySelector('input[name=zipCode]');
+        var val = zipcode.value;
+        return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(val);
+    }
+
+    function _isValidPhone() {
+        return _isNum(areaCode) && _isNum(firstNum) && _isNum(secondNum);
+    }
+
+    function _alert(message) {
+        alert(message);
+    }
+
+})();
