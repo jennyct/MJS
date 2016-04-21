6d11218674d4c586269a6069120ffc7c0caa1d59
jennyct <jennychungtang@gmail.com>
2016-04-20 22:35:14 -0700

    Create variables for POST method

---
+++ orders.php
@@ -0,0 +1,15 @@
+<?php 
+	$quantity = $_POST['quantity'];
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$phoneNumber = $_POST['phoneNumber'];
+	$address = $_POST['address'];
+	$city = $_POST['city'];
+	$zipCode = $_POST['zipCode'];
+	$shippingMethod = $_POST['shippingMethod'];
+	$ccNumber = $_POST['ccNumber'];
+	$ccExp = $_POST['ccExp'];
+	$ccCSC = $_POST['ccCSC'];
+
+	
+?>
