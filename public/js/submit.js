bad923eea5d4693fa40c3096792f102a7c28692d
jennyct <jennychungtang@gmail.com>
2016-04-27 16:17:41 -0700

    Moved things around

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-	$('form').submit(function(event){
-		event.preventDefault();
-		var formData = $(this).serializeArray();
-		$.ajax({
-			type : "POST", 
-			url : 'orders.php',
-			data : formData,
-			success : addOrder
-		});
-		console.log(this);
-	})
+	
 });
 
-var addOrder = function(data){
-	alert("Order processed successfully!");
-	console.log(data);
-};
